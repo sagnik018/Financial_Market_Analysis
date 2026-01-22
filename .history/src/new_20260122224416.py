9eaa8a442ef26b2efc42a99b222097d86459ee7c
Sagnik Saha <140305361+sagnik018@users.noreply.github.com>
2026-01-22 22:45:01 +0530

    Added all the new and modified files

--- .history/src/new_20260122224416.py
+++ .history/src/new_20260122224416.py
@@ -1,4 +1,4 @@
-# FOR demo only
+
 import yfinance as yf
 stock =yf.Ticker("AAPL")
 df = stock.history(period="2y")
--- .history/src/new_20260122224426.py
+++ .history/src/new_20260122224426.py
@@ -1,4 +1,4 @@
-# FOR demo only
+# FOR de
 import yfinance as yf
 stock =yf.Ticker("AAPL")
 df = stock.history(period="2y")
